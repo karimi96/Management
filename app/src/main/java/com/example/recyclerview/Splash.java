10440cdd22e75c4f3a00c41f37300df55d2ed447
roghaye <zmanyamyd5@gmail.com>
2021-09-13 08:37:53 +0430

    add cardview

--- app/src/main/java/com/example/recyclerview/Splash.java
+++ app/src/main/java/com/example/recyclerview/Splash.java
@@ -1,14 +1,30 @@
 package com.example.recyclerview;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+
+            }
+        },3000);
+
+
     }
 }
